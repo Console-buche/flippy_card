e36bc383fd7d9ea317b00368223fdc03f6a0fbbb
eightbitsofdata <dubourg.sebastien@gmail.com>
2021-02-20 02:02:44 +0100

    C'est parti pour la flippy card

---
+++ README.md
@@ -0,0 +1,66 @@
+  <h3 align="center">Flip card à décliner</h3>
+
+  <p align="center">
+    Cette flip card est d'abord un simple projet CSS, le helloworld des effets rigolos qui marchent toujours aussi bien.
+    Carre carte sert de template de départ pour des exp/projets qui en font l'usage : animations, composants js...
+    <br />
+    <a href="https://github.com/github_username/repo_name"><strong>Explore the docs »</strong></a>
+    <br />
+    <br />
+    <a href="https://github.com/github_username/repo_name">View Demo</a>
+    ·
+    <a href="https://github.com/github_username/repo_name/issues">Report Bug</a>
+    ·
+    <a href="https://github.com/github_username/repo_name/issues">Request Feature</a>
+  </p>
+</p>
+
+<!-- TABLE OF CONTENTS -->
+<details open="open">
+  <summary><h2 style="display: inline-block">Table of Contents</h2></summary>
+  <ol>
+    <li><a href="#roadmap">A faire</a></li>
+    <li><a href="#license">License</a></li>
+    <li><a href="#contact">Contact</a></li>
+    <li><a href="#acknowledgements">Inspi</a></li>
+  </ol>
+</details>
+
+<!-- A propos  -->
+
+## A propos
+
+2021 c'est décidé, j'arrête de pourrir mon ordinateur avec des tas de dossiers projets. Je passe à git, parce que je me dis qu'après tout, c'est cool de partager. Et puis ça m'évitera aussi de tout fourrer sur un FTP dès que je change de lieu (éh oui, 2021 et je fais encore ça, shaaaame) :P
+
+Hum.
+Et donc à propos de ce projet : une web exp, une expérience, une production temps libre... A décliner (et qui ne se perdra plus dans/un/dossier/random/\_\_sur/\_MON_FTP/quelquepart/en9898878v.xyz)
+
+<!-- Todo -->
+
+## Todo
+
+- Finir le travail sur le CSS
+- Automatiser l'animation
+- Tester, tester, tester
+- Nettoyer le code
+
+- Fork à venir : countdown dd/jj/mm/ss
+- Fork à venir : Vue.js
+
+<!-- LICENSE -->
+
+## License
+
+MIT et tout et tout. Servez-vous :-)
+
+<!-- CONTACT -->
+
+## Contact
+
+Sébastien Dubourg (Eightbitsofdata) - [@eightbitsofdata](https://twitter.com/eightbitsofdata) - email
+
+<!-- Inspi -->
+
+## Inspi
+
+- [Frontend Mentor](https://www.frontendmentor.io/challenges/)
---
+++ src/index.js
@@ -0,0 +1,74 @@
+import "./scss/main.scss";
+
+
+/* composant carte */
+//state de la carte :
+let cardCounter = 0;
+let lacarte = document.getElementById("card")
+let shady = document.getElementById("shader")
+
+// DEV
+// Déclenchement manuel
+let button = document.getElementById("zou")
+button.addEventListener('click', () => {
+    lacarte.classList.add('anim1')
+    shady.classList.add('animdarken')
+}, false)
+
+
+
+//listeners pour l'enchaînement des anim
+lacarte.addEventListener("animationstart", (e) => {
+    //Lorsqu'une carte se tourne, on commence par update le top du numéro arrière
+    if (e.animationName == "tick") {
+        updateCounter("c_behind", "counttop")
+        
+    } 
+})
+
+lacarte.addEventListener("animationend", (e) => {
+    //Hop on store le nom de l'anim qui se termine
+    let animName = e.animationName;
+
+    //Si c'est la première moitié de flip
+    if (animName == "tick") {
+
+    //alors ben on retire l'anim (puisqu'elle est finie quoi, d'uh)
+    lacarte.classList.remove('anim1');
+    //on en profite pour mettre à jour le compteur du dos de la carte
+    updateCounter("c_back")
+
+    //et on lance la seconde moitié du flip
+    lacarte.classList.add('anim2')
+
+    } else if (animName == "tack") {
+        //dans le cas où la deuxième moitié du flip est terminée, on retire l'anim
+        lacarte.classList.remove('anim2')
+        //reset de l'ombre portée sur le bas par le volet du haut (damn jamais je me comprendrai à relire ça d'ici quelques mois)
+        shady.classList.remove('animdarken')
+        //on met à jour le bas de la carte, pré-animation
+        updateCounter("c_behind", "countbottom")
+        
+        //enfin on met à jour le front du haut avant de relancer la boucle
+        updateCounter("c_front")
+
+    
+    }
+})
+
+
+
+//UTILS : increment counter
+//simple fonction pour incr le compteur en se basant sur le c_behind qui passe ensuite en state
+//les attrs sont tous mis à jour un à un puisqu'utilisés pour display le compteur via le css
+function updateCounter(target, atr = "count") {
+    const elem = document.getElementById(target);
+    let getcount = elem.getAttribute(atr)
+    let newCount = parseInt(getcount, 10) + 1;
+    if (target == "c_behind") {
+         cardCounter = newCount;
+        elem.setAttribute(atr, cardCounter)
+    } else {
+        elem.setAttribute(atr, cardCounter)
+    }
+}
